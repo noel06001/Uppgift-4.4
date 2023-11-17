e50144d78eb1ae747b7392d29cdd62d668c2c40b
Admin <Admin@TEB123013>
2023-11-17 15:18:25 +0100

    Add project files.

---
+++ Uppgift 4.4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 40; i < 81; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
